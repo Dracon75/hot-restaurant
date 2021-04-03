7c1bbb9738fffc32ea595d9458a04947c6dac7b0
bborumoore <bborumoore@gmail.com>
2021-04-03 10:54:28 -0400

    Set up initial attempt at server

--- server.js
+++ server.js
@@ -12,74 +12,74 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Star Wars Characters (DATA)
+// Reserved Tables (DATA)
 
-const tables = [
-  {
-    routeName: 'table1',
-    name: 'Yoda',
-    role: 'Jedi Master',
-    age: 900,
-    forcePoints: 2000,
-  },
-  {
-    routeName: 'darthmaul',
-    name: 'Darth Maul',
-    role: 'Sith Lord',
-    age: 200,
-    forcePoints: 1200,
-  },
-  {
-    routeName: 'obiwankenobi',
-    name: 'Obi Wan Kenobi',
-    role: 'Jedi Master',
-    age: 55,
-    forcePoints: 1350,
-  },
-];
+const reservations = [];
+
+const waitingList = [];
 
 // Routes
 
-// Basic route that sends the user first to the AJAX Page
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
+// Basic routing stuff
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
 
-app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'add.html')));
+app.get('/make', (req, res) => res.sendFile(path.join(__dirname, 'make.html')));
 
-// Displays all characters
-app.get('/api/characters', (req, res) => res.json(characters));
+app.get('/view', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
 
-// Displays a single character, or returns false
-app.get('/api/characters/:character', (req, res) => {
-  const chosen = req.params.character;
+// Displays all reservations
+app.get('/api/reservations', (req, res) => res.json(reservations));
 
-  console.log(chosen);
+// Displays all waiting list
+app.get('/api/waitingList', (req, res) => res.json(waitingList));
 
-  /* Check each character routeName and see if the same as "chosen"
-   If the statement is true, send the character back as JSON,
-   otherwise tell the user no character was found */
+// // Displays a single character, or returns false
+// app.get('/api/characters/:character', (req, res) => {
+//   const chosen = req.params.character;
 
-  for (let i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
+//   console.log(chosen);
 
-  return res.json(false);
-});
+//   /* Check each character routeName and see if the same as "chosen"
+//    If the statement is true, send the character back as JSON,
+//    otherwise tell the user no character was found */
 
-// Create New Characters - takes in JSON input
-app.post('/api/characters', (req, res) => {
+//   for (let i = 0; i < characters.length; i++) {
+//     if (chosen === characters[i].routeName) {
+//       return res.json(characters[i]);
+//     }
+//   }
+
+//   return res.json(false);
+// });
+
+// Create New Table Reservation - takes in JSON input
+app.post('/api/reservation', (req, res) => {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
-  const newCharacter = req.body;
+  if(reservations.length < 5) {
+    const newReservation = req.body;
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, '').toLowerCase();
-  console.log(newCharacter);
+  newReservation.custonerName = newReservation.custonerName.replace(/\s+/g, '').toLowerCase();
+  newReservation.custonerEmail = newReservation.custonerEmail.replace(/\s+/g, '').toLowerCase();
+  console.log(newReservation);
 
-  characters.push(newCharacter);
-  res.json(newCharacter);
+  reservations.push(newReservation);
+  res.json(newReservation);
+  } else {
+    const newWaitingList = req.body;
+
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newWaitingList.custonerName = newWaitingList.custonerName.replace(/\s+/g, '').toLowerCase();
+    newWaitingList.custonerEmail = newWaitingList.custonerEmail.replace(/\s+/g, '').toLowerCase();
+    console.log(newWaitingList);
+  
+    waitingList.push(newWaitingList);
+    res.json(newWaitingList);
+  }
+  
 });
 
 // Starts the server to begin listening
